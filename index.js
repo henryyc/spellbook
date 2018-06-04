2f935d7550256b16b9b341056589c130251c3448
Henry Chen <hyc@umd.edu>
2018-06-04 17:25:48 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("testing")
+console.log("testing");
+
+function changeText() {
+	document.getElementById("title-two").textContent = "Finite Incantatum";
+}
+
+const button = document.querySelector("button");
+document.querySelector("button").addEventListener("click", changeText);
+
+const form = document.querySelector("form");
+function dostuff(e){
+  e.preventDefault();
+  document.getElementById("title-three").textContent = form.querySelector("input").value;
+  return false;
+}
+
+form.addEventListener("submit", dostuff);
