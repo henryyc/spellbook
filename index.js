cacc4633d98440566e7987f5bdbe145a74b31b86
Henry Chen <hyc@umd.edu>
2018-06-05 18:03:37 -0400

    Finished HW2

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-console.log("testing");
-
-function changeText() {
-	document.getElementById("title-two").textContent = "Finite Incantatum";
-}
-
-const button = document.querySelector("button");
-document.querySelector("button").addEventListener("click", changeText);
-
 const form = document.querySelector("form");
-function dostuff(e){
+const avgSpellLength = 10.2; //src: https://arxiv.org/pdf/1208.6109.pdf, multiplied by 2
+const manaRange = document.getElementById("mCost").max - document.getElementById("mCost").min + 1;
+
+function taskExecution(e) {
   e.preventDefault();
-  document.getElementById("title-two").textContent = form.querySelector("input").value;
-  return false;
+
+	if (document.getElementById("sName").value != "") {
+		//div to display both spell and mana info as one item on the list
+		const info = document.createElement("li");
+
+		getSpellInfo(info);
+	}
 }
 
-form.addEventListener("submit", dostuff);
+//create span to display spell name
+function getSpellInfo(info) {
+	const spellInfo = document.createElement("span");
+	spellInfo.className = "spellName";
+
+	const spellName = document.getElementById("sName").value;
+	spellInfo.appendChild(document.createTextNode(spellName + ": \t"));
+	spellInfo.style.color = "rgb(" + spellName.length/avgSpellLength*255 + ", 0, 0)";
+
+	getManaInfo(info, spellInfo);
+}
+
+//create span to display mana cost
+function getManaInfo(info, spellInfo) {
+	const manaInfo = document.createElement("span");
+	manaInfo.className = "manaCost";
+
+	const manaCost = document.getElementById("mCost").value;
+	manaInfo.appendChild(document.createTextNode(manaCost + " mana"));
+	manaInfo.style.color = "rgb(0, 0, " + manaCost/manaRange*255 + ")";
+
+	wrapUp(info, spellInfo, manaInfo);
+}
+
+function wrapUp(info, spellInfo, manaInfo) {
+	//add all info to the list
+	info.appendChild(spellInfo);
+	info.appendChild(manaInfo);
+	document.getElementById("spells").appendChild(info);
+
+	//soft reset form
+	//document.getElementById("sName").value = '';
+}
+
+form.addEventListener("submit", taskExecution);
